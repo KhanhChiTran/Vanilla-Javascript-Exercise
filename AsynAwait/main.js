02985c93c2d357ffbc4f5303d846d1809a6ccb25
ChiTran <Khanhchi.tran13051@gmail.com>
2021-04-01 21:53:25 +0200

    ES 5 function example

--- AsynAwait/main.js
+++ AsynAwait/main.js
@@ -1,9 +1,21 @@
-const url = "https://api.github.com/users/nrcool/repos";
+// API Url
+// const url = "https://api.github.com/users/nrcool/repos";
 
-const fetchData = async () => {
+// const fetchData = async () => {
+//   const respond = await fetch(url);
+//   console.log("You are here waiting for me");
+//   const data = await respond.json();
+
+//   console.log(data[0]);
+// };
+// fetchData();
+// console.log("outside waiting for me... ");
+
+async function fetchData() {
   const respond = await fetch(url);
   const data = await respond.json();
-
-  console.log("You are here waiting for me");
+  console.log("Before get Data");
   console.log(data);
-};
+  console.log("Hello world");
+}
+fetchData();
